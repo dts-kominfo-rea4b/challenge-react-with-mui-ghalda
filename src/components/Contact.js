5ef645352192e242b6cdf19aa7ab5bdd547e58b0
ghalda <gputribalqis@gmail.com>
2022-09-27 21:05:53 +0700

    contact list components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,38 @@
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
 import React from 'react';
+import {
+	Avatar,
+	ListItem,
+	ListItemAvatar,
+	ListItemText,
+	Typography,
+} from "@mui/material";
 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
 const Contact = ({ data }) => {
     // Contact berisi foto, nama, telepon, dan email
-    return (<></>);
+    return (
+    <>
+        <ListItem>
+			<ListItemAvatar>
+				<Avatar src={data.photo} />
+			</ListItemAvatar>
+			<ListItemText
+				primary={data.name}
+				secondaryTypographyProps={{ component: "div" }}
+				secondary={
+					<React.Fragment>
+						<Typography>{data.phone}</Typography>
+						<Typography>{data.email}</Typography>
+					</React.Fragment>
+				}
+				title={data.name}
+			/>
+		</ListItem>
+    </>
+    );
 };
 
 export default Contact;
