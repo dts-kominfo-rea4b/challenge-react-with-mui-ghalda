1c71894a41d6a6304e45388c96762f377a8fbd71
ghalda <gputribalqis@gmail.com>
2022-09-27 21:05:04 +0700

    header components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 // atau langsung tambahkan dengan sx={{}}
 import React from 'react';
+import {Divider, Typography} from "@mui/material";
 
 // Header berisi judul dan subtitle
 // Judul: Call a Friend
 // Subtitle: Your friendly contact app
 const Header = () => {
   return (
-    <></>
+    <>
+        <Typography variant="h2" gutterBottom>
+          Call A Friend
+        </Typography>
+        <Divider sx={{ mb: 5}}>
+        Your friendly contact app
+        </Divider>
+    </>
   );
 };
 
